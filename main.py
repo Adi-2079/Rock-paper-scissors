bcedc8c13808c4813fa42508535ead6ab6759d75
Adi-2079 <aditya.mukhija@education.nsw.gov.au>
2024-07-23 09:29:53 +1000

    Game part 2

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from random import randint
 
-t = ["Rock, Paper, Scissors"]
-computer = t[randint(0,2)]
+t = ["Rock", "Paper", "Scissors"]
+computer = t[randint(0, 2)]
 player = False
 
 while player == False:
-    print ("Lets play rock paper scissors")
-    player = input["Rock", "Paper", "Scissors"]
+    print("Let's play Rock, Paper, Scissors")
+    player = input("Rock", "Paper", "Scissors ")
+    
     if player == computer:
-        print ("Tie")
-    elif player == ("Rock"):
-        if computer == ("Paper"):
-            print ("You lost, computer picked paper!")
-        elif computer == ("Scissors"):
-            print("You won, computer picked Scissors")
-    elif player == ("Paper"):
-        if computer == ("Scissors"):
-            print("You lost, computer picked scissors")
-        elif computer == ("Rock"):
-            print("You won! Computer picked Rock!")
-    elif player == ("Scissors"):
-        if computer == ("Rock"):
-            print ("You lost! Computer picked rock!")
-        elif computer == ("Paper"):
-            print ("You won, computer picked paper!")
+        print("It's a tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lost! Computer picked Paper.")
+        elif computer == "Scissors":
+            print("You won! Computer picked Scissors.")
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lost! Computer picked Scissors.")
+        elif computer == "Rock":
+            print("You won! Computer picked Rock.")
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lost! Computer picked Rock.")
+        elif computer == "Paper":
+            print("You won! Computer picked Paper.")
     else:
         print("That's not a valid play. Check your spelling!")
-    #player was set to True, but we want it to be False so the loop continues
-player = False
-computer = t[randint(0,2)]
+
+    # Reset player to False so the loop can continue
+    player = False
+    computer = t[randint(0, 2)]
